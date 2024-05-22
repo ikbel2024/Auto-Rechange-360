d80aafdbf73828c89fd75535c14eb5b8d116f7bf
akrem4500 <133696548+akrem4500@users.noreply.github.com>
2024-05-22 14:58:27 +0100

    signup

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.signup_get = (req,res)=> {
     res.render('signup'); 
 }
@@ -5,9 +7,23 @@ module.exports.signup_get = (req,res)=> {
 module.exports.login_get = (req,res)=> {
     res.render('login'); 
 }
-module.exports.signup_post = (req,res)=> {
-    res.send('new singup'); 
+module.exports.signup_post = async (req,res)=> {
+    const { nom , prenom , adresse , email,num_tel, mot_de_passe , roleId } = req.body ; 
+    try{
+
+        const user = await User.create({nom , prenom , adresse , email,num_tel, mot_de_passe , roleId}); 
+        res.status(201).json(user); 
+
+    }
+    catch (err){
+        console.log(err); 
+        res.status(400)('error , user not created !'); 
+    }
 }
-module.exports.login_post = (req,res)=> {
+module.exports.login_post = async (req,res)=> {
+
+    const { nom , prenom , adresse , email,num_tel, mot_de_passe , roleId } = req.body ; 
+
+    console.log(nom , prenom , adresse , email,num_tel, mot_de_passe , roleId);
     res.send('user login'); 
 }
