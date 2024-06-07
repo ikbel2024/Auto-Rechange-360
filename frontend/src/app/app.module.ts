5a143f1d5c928a3cb27b4ca45b2c31054b3f7446
Nizarmlaouihi <100137882+Nizarmlaouihi@users.noreply.github.com>
2024-06-07 17:48:53 +0100

    update 1

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,73 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProdComponent } from './Product/detail-prod/detail-prod.component';
+
+import { AddProductComponent } from './Product/add-product/add-product.component';
+
+import { TvComponent } from './categorie/Stock-Details/tv.component';
+
+import { ShowOneProductComponent } from './show-one-product/show-one-product.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AdministrationComponent } from './administration/administration.component';
+import { DeleteProductComponent } from './Product/delete-product/delete-product.component';
+import { UpdateProductComponent } from './Product/update-product/update-product.component';
+import { ShowProductsComponent } from './Product/show-products/show-products.component';
+import { SearchProductsComponent } from './Product/search-products/search-products.component';
+import { CountProductComponent } from './Product/count-product/count-product.component';
+import { FooterComponent } from './footer/footer.component';
+import { AddCategorieComponent } from './categorie/add-categorie/add-categorie.component';
+import { DeleteCategorieComponent } from './categorie/delete-categorie/delete-categorie.component';
+import { UpdateCategorieComponent } from './categorie/update-categorie/update-categorie.component';
+import { ShowCategorieComponent } from './categorie/show-categorie/show-categorie.component';
+import { SearchCategorieComponent } from './categorie/search-categorie/search-categorie.component';
+import { CountCategorieComponent } from './categorie/count-categorie/count-categorie.component';
+import { ChatBotComponent } from './chat-bot/chat-bot.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+    NavbarComponent,
+    NotFoundComponent,
+    DetailProdComponent,
+    AddProductComponent,
+    TvComponent,
+
+    ShowOneProductComponent,
+
+    AdministrationComponent,
+    DeleteProductComponent,
+    UpdateProductComponent,
+    ShowProductsComponent,
+    SearchProductsComponent,
+    CountProductComponent,
+    FooterComponent,
+    AddCategorieComponent,
+    DeleteCategorieComponent,
+    UpdateCategorieComponent,
+    ShowCategorieComponent,
+    SearchCategorieComponent,
+    CountCategorieComponent,
+    ChatBotComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ frontend/src/app/services/consumer-product.service.ts
@@ -0,0 +1,71 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConsumerProductService {
+
+ // Placeholder method for searching product by name
+ searchProductByName(productName: string): Observable<Product[]> {
+  return this.http.get<Product[]>(`${this.apiUrl}/search/${productName}`);
+}
+  private apiUrl = 'http://localhost:3000/ProduitR';
+
+  constructor(private http: HttpClient) { }
+
+  // Get all products
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}/show`);
+  }
+
+  // Get product by ID
+  getProductById(id: number): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/find/${id}`);
+  }
+
+  addProduct(formData: FormData): Observable<any> {
+    return this.http.post(`${this.apiUrl}/add`, formData);
+  }
+
+  /*
+  // Add a product
+  addProduct(product: Product): Observable<any> {
+    return this.http.post(`${this.apiUrl}/add`, product);
+  }*/
+
+  addProductP(productData: FormData): Observable<any> {
+    const headers = new HttpHeaders({
+      // 'Content-Type': 'multipart/form-data'  // Do not set Content-Type, browser will set it automatically
+    });
+
+    return this.http.post(`${this.apiUrl}/addPic`, productData, { headers });
+  }
+
+  // Delete a product by ID
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/delete/${id}`);
+  }
+
+  deleteProductByName(productName: string): Observable<any> {
+    const url = `${this.apiUrl}/deleteByName/${encodeURIComponent(productName)}`;
+    return this.http.delete<any>(url);
+  }
+
+  // Update a product
+  updateProduct(product: Product, id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/update/${id}`, product);
+  }
+
+  // Find product by name
+  findProduitName(name: string): Observable<Product> {
+    return this.http.get<Product>(`${this.apiUrl}/name/${name}`);
+  }
+
+  // Method to count products by category
+  countProductsByCategory(category: string): Observable<{ count: number }> {
+    return this.http.get<{ count: number }>(`${this.apiUrl}/countp?category=${category}`);
+  }
+}
