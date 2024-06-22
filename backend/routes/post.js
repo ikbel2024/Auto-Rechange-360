d4b986282c304adabc73d36a0b6ccae25690db41
hazemhendi <150842521+hazemhendi@users.noreply.github.com>
2024-06-22 12:53:18 +0200

    add the greeting api

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var Post = require('../model/post');
 var postcontroller = require('../controller/postcontroller');
 const validate=require('../middl/validate');
+const checkGreetings = require('../middl/greetingCheck');
 
 
 router.get('/show', postcontroller.show);
-router.post('/add',validate.validatepost, postcontroller.add);
-router.put('/update/:id', postcontroller.update);
+router.post('/add',validate.validatepost, checkGreetings, postcontroller.add);
+router.put('/update/:id', postcontroller.update, checkGreetings);
 router.delete('/delete/:id', postcontroller.dlete);
 
 router.get('/postid/:id', postcontroller.findid);
