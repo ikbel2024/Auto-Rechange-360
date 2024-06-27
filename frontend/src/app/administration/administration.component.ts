fc588bae54501ef571f77444537fdc3d9acef9d8
Jihene Khelifi <jihene.khelifi@esprit.tn>
2024-06-27 22:00:42 +0100

    Dashboard mal présenté

--- frontend/src/app/administration/administration.component.ts
+++ frontend/src/app/administration/administration.component.ts
@@ -1,15 +1,36 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
-
+import { Vehicule } from '../model/vehicule';
+import { VehiculeService } from '../services/vehicule.service';
 
 @Component({
   selector: 'app-administration',
   templateUrl: './administration.component.html',
   styleUrls: ['./administration.component.css']
 })
-export class AdministrationComponent  {
-  constructor() {}
+export class AdministrationComponent implements OnInit {
 
- 
+  vehicules: Vehicule[] = [];
+  constructor(private vehiculeService: VehiculeService) {}
+
+  ngOnInit(): void {
+    this.loadVehicules();
+  }
+
+  loadVehicules(): void {
+    this.vehiculeService.getVehicules().subscribe(
+      (data: Vehicule[]) => {
+        this.vehicules = data;
+      },
+      error => {
+        console.error('Error fetching vehicules:', error);
+      }
+    );
+  }
+
+   // Méthode pour calculer la largeur de la barre de progression
+   getProgressWidth(prix: number): string {
+    return prix + '%';
+  }
+
 }
