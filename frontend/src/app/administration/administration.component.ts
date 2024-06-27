14460b3a2471b8bac141815226c5d89fe58e5ab2
Jihene Khelifi <jihene.khelifi@esprit.tn>
2024-06-27 21:31:55 +0100

    Dashboard

--- frontend/src/app/administration/administration.component.ts
+++ frontend/src/app/administration/administration.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+
 
 @Component({
   selector: 'app-administration',
   templateUrl: './administration.component.html',
   styleUrls: ['./administration.component.css']
 })
-export class AdministrationComponent {
+export class AdministrationComponent  {
+  constructor() {}
 
+ 
 }
